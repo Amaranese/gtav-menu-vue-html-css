92efb465a516555cc69a56f6605776b139a3d6d7
Amaranese <alejandro.maranes@asendia.com>
2021-08-04 23:14:41 +0200

    .

---
+++ src/components/selection.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul class="selection">
+    <li v-for="(item, ind) in list" v-bind:class="{ active: index === ind }">
+			{{ item }}
+		</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+		index: {
+			type: Number,
+			default: 0
+		},
+		list: Array
+	}
+}
+</script>
